0af6f832d48ec7a6bdb6442f6eb88e8063f19f8a
Kasra Abdollahi <kasra.ab@mtnirancell.ir>
2025-05-12 10:19:11 +0330

    another version; without prompting

---
+++ main-without-prompt.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"sort"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: go run main.go <matchTag> <newTag> <finalNestKey> <path_to_json_file>")
+		os.Exit(1)
+	}
+
+	matchTag := os.Args[1]
+	newTag := os.Args[2]
+	finalNestKey := os.Args[3]
+	filePath := os.Args[4]
+
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read file: %v\n", err)
+		os.Exit(1)
+	}
+
+	var input map[string]interface{}
+	if err := json.Unmarshal(data, &input); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse JSON: %v\n", err)
+		os.Exit(1)
+	}
+
+	output := []string{
+		"[FILTER]",
+		"  Name          rewrite_tag",
+		fmt.Sprintf("  Match         %s", matchTag),
+		"  Rule          $service_log['event'] ^(access).* " + newTag + " true",
+		"  Emitter_Name  re_emitted",
+		"",
+	}
+
+	flatKeys := map[string]bool{}
+	extractNestedKeys(input, "", flatKeys)
+
+	keys := make([]string, 0, len(flatKeys))
+	for k := range flatKeys {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		output = append(output, "[FILTER]")
+		output = append(output, "  Name nest")
+		output = append(output, fmt.Sprintf("  Match %s", newTag))
+		output = append(output, "  Operation lift")
+		output = append(output, fmt.Sprintf("  Nested_under %s", key))
+		output = append(output, fmt.Sprintf("  Add_prefix %s_", key))
+		output = append(output, "")
+	}
+
+	output = append(output, "[FILTER]")
+	output = append(output, "  Name nest")
+	output = append(output, fmt.Sprintf("  Match %s", newTag))
+	output = append(output, "  Operation nest")
+	output = append(output, "  Wildcard service_log_*")
+	output = append(output, fmt.Sprintf("  Nest_under %s", finalNestKey))
+
+	if err := ioutil.WriteFile("filters.conf", []byte(strings.Join(output, "\n")), 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write filters.conf: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("filters.conf generated successfully.")
+}
+
+func extractNestedKeys(data interface{}, prefix string, result map[string]bool) {
+	m, ok := data.(map[string]interface{})
+	if !ok {
+		return
+	}
+	for k, v := range m {
+		fullKey := k
+		if prefix != "" {
+			fullKey = prefix + "_" + k
+		}
+		valKind := reflect.TypeOf(v).Kind()
+		if valKind == reflect.Map {
+			result[fullKey] = true
+			extractNestedKeys(v, fullKey, result)
+		} else if valKind == reflect.Slice {
+			slice := v.([]interface{})
+			for _, item := range slice {
+				extractNestedKeys(item, fullKey, result)
+			}
+		}
+	}
+}
